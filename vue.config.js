a500cef51b94203dc0fd208dd3d42af22ceefb41
bagakibadi <solaybagus2@gmail.com>
2021-09-05 15:49:21 +0700

    tambah PWA

---
+++ vue.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Portofolio Bagus',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'logo.ico',
+      favicon16: 'logo.ico',
+      appleTouchIcon: 'logo.ico',
+      maskIcon: 'logo.ico',
+      msTitleImage: 'logo.ico',
+    },
+    // workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc: 'src/service-worker.js',
+      skipWaiting: true,
+    },
+    manifestOptions: {
+      gcm_sender_id: '522489256827',
+      icons: [
+        {
+          src: 'logo.ico',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'logo.ico',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  },
+};
