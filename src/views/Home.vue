839341283cc8062d4f49f616c0df76b33eb4b99c
fari <ahmadzhofari_az86@gmail.com>
2023-02-04 21:10:51 +0700

    add profile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -57,40 +57,39 @@
                         <h2>Hi, I’m <br> <span>Ahmad Zhofari</span></h2>
                       </div>
                       <div class="text-desc mb-4">
-                        <p>I am a Front End developer with experience in web development using
-                          modern technology like Vue.Js, React Js, Node Js and Express Js. Able
-                          work in Team and have good communication in Team, Fast Learner
-                          new think and love challanges.</p>
+                        <p>
+                          I am a 4th-semester student of Educational Technology at Yogyakarta State University. I have a strong passion for Design and videography and am ambitious to pursue my goals as an Instructor and designer. However, I faced financial constraints in completing my education. Therefore, I hope to receive a scholarship from KarirLab to help pay for my tuition and achieve my academic goals. I am sure that I can make good use of this opportunity and make a positive contribution to society after completing my education
+                        </p>
                       </div>
                       <div>
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                          Front End Web Developer & Back End Developer
+                          Graphic Design and Videography
                         </div>
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
-                          solaybagus2@gmail.com
+                          ahmadzhofari_az86@gmail.com
                         </div>
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                          +62 851 5617 9875
+                          +62 851 5605 4633
                         </div>
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-                          <a href="https://github.com/bagakibadi" style="text-decoration: none" target="_blank">
-                            bagakibadi
+                          <a href="https://github.com/Ahmadzhofari99" style="text-decoration: none" target="_blank">
+                            Ahmadzhofari99
                           </a>
                         </div>
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
-                          <a href="https://linkedin.com/in/bagusnursolayman" style="text-decoration: none" target="_blank">
-                            bagusnursolayman
+                          <a href="https://www.linkedin.com/in/ahmad-zhofari-08a86b217" style="text-decoration: none" target="_blank">
+                            Ahmad Zhofari
                           </a>
                         </div>
 
                         <div class="d-flex info">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff014f" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-                          DKI Jakarta, Yogyakarta
+                          Yogyakarta
                         </div>
                       </div>
                     </div>
@@ -102,12 +101,12 @@
                       </div>
                       <div class="row">
                         <div class="col-sm-6 col-12 mb-3">
-                          <a href="/CV-BagusNurSolayman.pdf" target="_blank" class="btn btn-dark w-100 ">
+                          <a href="/CV_Ahmad_Zhofari.pdf" target="_blank" class="btn btn-dark w-100 ">
                             Download CV
                           </a>
                         </div>
                         <div class="col-sm-6 col-12 mb-3">
-                          <a href="https://wa.me/6285156179875" target="_blank" class="btn btn-dark w-100">
+                          <a href="https://wa.me/6285156054633" target="_blank" class="btn btn-dark w-100">
                             My Contact
                           </a>
                         </div>
